812800fac9fd253d9c7a487822c3e3e9dd9859a6
AnthonyMRL <am4866692@gmail.com>
2025-05-01 08:15:16 -0500

    Creacion de los controladores

---
+++ MoralesA_ExamenProgramacion/Data/MoralesA_ExamenProgramacionContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MoralesA_ExamenProgramacion.Models;
+
+namespace MoralesA_ExamenProgramacion.Data
+{
+    public class MoralesA_ExamenProgramacionContext : DbContext
+    {
+        public MoralesA_ExamenProgramacionContext (DbContextOptions<MoralesA_ExamenProgramacionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MoralesA_ExamenProgramacion.Models.Propietario> Propietario { get; set; } = default!;
+        public DbSet<MoralesA_ExamenProgramacion.Models.Mascota> Mascota { get; set; } = default!;
+        public DbSet<MoralesA_ExamenProgramacion.Models.Visita> Visita { get; set; } = default!;
+    }
+}
