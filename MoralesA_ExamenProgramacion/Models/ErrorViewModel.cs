01d517969a0d6709732af0bfd6fba75305ae6b83
AnthonyMRL <am4866692@gmail.com>
2025-05-01 07:12:22 -0500

    Agregar archivos de proyecto.

---
+++ MoralesA_ExamenProgramacion/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoralesA_ExamenProgramacion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
